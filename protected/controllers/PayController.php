388a66f5bdeef72242db53b765eafc6f380d6aad
andrew2net <andriano@ngs.ru>
2014-10-20 01:50:32 +0700

    Revert ""
    This reverts commit 360a6c9ffd723bd362b8a048fb30605b2af4bbe9.

--- protected/controllers/PayController.php
+++ protected/controllers/PayController.php
@@ -78,7 +78,7 @@ class PayController extends Controller {
       $profile = new Profile;
       $profile->user_id = $uid;
     }
-    Yii::trace('before phone', 'pay_notify');
+    Yii::trace(is_null($profile), 'pay_notify');
     $profile->mobile_phone = $_POST['sender_phone'];
     $profile->save();
     Yii::trace('after phone', 'pay_notify');
