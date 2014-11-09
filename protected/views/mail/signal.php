15e64c67a25966eb713729d2871560536843da62
andrew2net <andriano@ngs.ru>
2014-11-10 04:50:54 +0700

    Revert ""
    This reverts commit 360a6c9ffd723bd362b8a048fb30605b2af4bbe9.

--- protected/views/mail/signal.php
+++ protected/views/mail/signal.php
@@ -14,6 +14,6 @@
         " " . $signal->sigdate . ' ' . $signal->recom . ' ' . number_format($signal->price, 2, '.', ' ');
         ?></p>
     <?php } ?>
-    $this->renderInternal(dirname(__FILE__) . '/_footer.php');
+    <?php $this->renderInternal(dirname(__FILE__) . '/_footer.php'); ?>
   </body>
 </html>
