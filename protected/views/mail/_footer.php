34553e385fad9c8019823a31309f5706b841af51
andrew2net <andriano@ngs.ru>
2014-11-10 03:50:20 +0700

    Revert ""
    This reverts commit 360a6c9ffd723bd362b8a048fb30605b2af4bbe9.

--- protected/views/mail/_footer.php
+++ protected/views/mail/_footer.php
@@ -1,4 +1,4 @@
-<p><a href="<?php echo Yii::app()->createAbsoluteUrl('profile'); ?>">Go to your private area</a></p>
+<p><a href="<?php echo Yii::app()->createAbsoluteUrl('private'); ?>">Go to your private area</a></p>
 <p>Thank you for choosing us!</p>
 <?php
 echo CHtml::tag('p', array(), 'This letter is generated automatically. Please do not reply to it.');
